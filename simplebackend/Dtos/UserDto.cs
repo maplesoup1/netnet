2b800f4cdf87a390bef31fa74a84ef28573af7e5
maplesoup1 <timmy000728@gmail.com>
2025-03-13 23:12:38 +1100

    Ignore simplebackend and unnecessary files

--- simplebackend/Dtos/UserDto.cs
+++
@@ -1,9 +0,0 @@
-namespace simplebackend.Dtos
-{
-    public class UserDto
-    {
-        public int Id { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-    }
-}
--- simplebackend/Dtos/UserUpdateDto.cs
+++
@@ -1,8 +0,0 @@
-namespace simplebackend.Dtos
-{
-    public class UserUpdateDto
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-    }
-}
